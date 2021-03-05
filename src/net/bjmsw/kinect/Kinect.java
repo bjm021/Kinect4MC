0757ca34e9cb0dddde36a205b629e6677d28b312
Benjamin J. Meyer <benmeyer@outlook.de>
2021-03-05 11:59:19 +0100

    Added License

--- src/net/bjmsw/kinect/Kinect.java
+++ src/net/bjmsw/kinect/Kinect.java
@@ -2,7 +2,22 @@ package net.bjmsw.kinect;
 
 import edu.ufl.digitalworlds.j4k.J4KSDK;
 
-
+/**
+ *     Copyright (C) 2021 BJM SoftwareWorks (Benjamin J. Meyer)
+ *
+ *     This program is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     This program is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
 public class Kinect extends J4KSDK {
 
     public Kinect() {
