300697360f50be6c21c550f234f11b9ca41d6fab
Benjamin J. Meyer <benmeyer@outlook.de>
2021-03-05 23:50:35 +0100

    Added License

--- src/net/bjmsw/kinect/ColorUtils.java
+++ src/net/bjmsw/kinect/ColorUtils.java
@@ -10,7 +10,23 @@ import java.util.ArrayList;
  * https://gist.github.com/nightlark/6482130#file-gistfile1-java (that has some errors) by Ryan Mast (nightlark)
  *
  * @author Xiaoxiao Li
+ * @author b.jm021
  *
+ *
+ *     Copyright (C) 2021 BJM SoftwareWorks (Benjamin J. Meyer)
+ *
+ *     This program is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     This program is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
 public class ColorUtils {
 
